474958d9586b0e5169b016a2546bfe58182a67a5
alyona-sazhina <alena_sajina@mail.ru>
2019-10-03 17:57:35 +0300

    updated main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -80,7 +80,7 @@ def write_to_file(path_to_file: str, content: tuple):
 
 frequencies = {}
 stop_words = ()
-top_n = 0
+top_n = 6
 lines_limit = top_n
 
 text = read_from_file("data.txt", lines_limit)
